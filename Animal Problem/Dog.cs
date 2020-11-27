209a2ecb98cff315e9bb5bc73eefc4d233e3cd41
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2020-11-27 23:45:19 +0530

    Add files via upload

---
+++ Animal Problem/Dog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ass2Ques2
+{
+    class Dog : Animal
+    {
+        //Properties
+        public override int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public override string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public override string Gender 
+        { 
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        //method
+        public Dog() { }
+        public Dog(int yr, string called,string gen)
+        {
+            this.age = yr;
+            this.name = called;
+            this.gender = gen;
+        }
+        public override void makesSound()
+        {
+            Console.WriteLine("Dog's Sound : Bhow!! Bhow!!");
+        }
+        public override void show()
+        {
+            Console.WriteLine("\nThe following are the Details of the Dog: ");
+            Console.WriteLine("Name: "+Name);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Gender: " + Gender);
+            makesSound();
+        }
+    }
+}
